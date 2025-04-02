3ad5cf99cff9eae863f76e98f5905ddc5e664e0f
Atakan Akşit <atakanaksit13211@gmail.com>
2025-04-02 14:43:30 +0300

    improve prototype

--- player.gd
+++ player.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
 signal hit
+signal shoot
 
 @export var speed = 400 # How fast the player will move (pixels/sec).
+@export var shooting_speed = 400 # How fast the player will shoot (delay ms between shots).
 var screen_size # Size of the game window.d
 
 func start(pos):
@@ -17,6 +19,23 @@ func _ready() -> void:
 	hide()
 
 
+func _load_config() -> void:
+	var config = ConfigFile.new()
+
+	# Load data from a file.
+	var err = config.load("user://global.cfg")
+
+	# If the file didn't load, ignore it.
+	if err != OK:
+		return
+
+	# Iterate over all sections.
+	for control_options in config.get_sections():
+		# Fetch the data for each section.
+		var isMouseMode = config.get_value(control_options, "isMouseMode")
+		
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var velocity = Vector2.ZERO # The player's movement vector.
@@ -28,6 +47,10 @@ func _process(delta: float) -> void:
 		velocity.y += 1
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
+	if Input.is_action_just_released("shoot"):
+		shoot.emit()
+	#if(): #mouse movement
+		
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
@@ -38,15 +61,20 @@ func _process(delta: float) -> void:
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
 	
-	if velocity.x != 0:
-		$AnimatedSprite2D.animation = "default"
-		$AnimatedSprite2D.flip_v = false
-		# See the note below about the following boolean assignment.
-		$AnimatedSprite2D.flip_h = velocity.x < 0
-		$AnimatedSprite2D.flip_v = velocity.y > 0 #make sure crossadswd movement down looks normal
-	elif velocity.y != 0:
-		$AnimatedSprite2D.animation = "default"
-		$AnimatedSprite2D.flip_v = velocity.y > 0
+	#if velocity.x != 0:
+	#	$AnimatedSprite2D.animation = "default"
+	#	$AnimatedSprite2D.flip_v = false
+	#	# See the note below about the following boolean assignment.
+	#	$AnimatedSprite2D.flip_h = velocity.x < 0
+	#	$AnimatedSprite2D.flip_v = velocity.y > 0 #make sure crossadswd movement down looks normal
+	#elif velocity.y != 0:
+	#	$AnimatedSprite2D.animation = "default"
+	#	$AnimatedSprite2D.flip_v = velocity.y > 0
+
+
+func get_bullet_spawn_point() -> Marker2D:
+	return $BulletSpawnPoint
+	
 
 
 func _on_body_entered(body: Node2D) -> void:
